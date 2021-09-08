71efed2a17c687815c53fba019ccf5b640f69253
yaseen1998 <86142826+yaseen1998@users.noreply.github.com>
2021-09-08 23:09:23 +0300

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,83 +1,47 @@
-/* eslint-disable no-lone-blocks */
 import React, { Component } from "react";
 import HornedBeast from "./HornedBeast";
 import img from "./data.json";
 class Image extends Component {
-    constructor(props){
-        super(props)
-      
-    }
+  constructor(props) {
+    super(props)
 
+  }
 
 
   render() {
     return (
       <div className="row">
-          
-        {img.map((element,index) => {
-           {
-                        img.map((element,index)=>{
-                          return <HornedBeast
-                          key ={index} 
-                          title={element.title} 
-                          image_url={element.image_url}
-                           description ={element.description}
-                          keyword = {element.keyword} 
-                          horns={element.horns}/>
-                        })
-                    }
-                    
-                </div>
 
-            
-            if(element.horns==this.props.Horns){
-          return <HornedBeast
-          key={index}
-            title={element.title}
-            image_url={element.image_url}
-            description={element.description}
-            keyword={element.keyword}
-            horns={element.horns}
-          />;
-        }
-          else if (this.props.Horns === ''){
-            
+        {img.map((element, index) => {
+
+          if (element.horns == this.props.Horns) {
             return <HornedBeast
-            key={index}
-            title={element.title}
-            image_url={element.image_url}
-            description={element.description}
-            keyword={element.keyword}
-            horns={element.horns}/>;
-           
+              key={index}
+              title={element.title}
+              image_url={element.image_url}
+              description={element.description}
+              keyword={element.keyword}
+              horns={element.horns}
+            />;
           }
-        
+          else if (this.props.Horns === '') {
+
+            return <HornedBeast
+              key={index}
+              title={element.title}
+              image_url={element.image_url}
+              description={element.description}
+              keyword={element.keyword}
+              horns={element.horns} />;
+
+          }
+
         })}
       </div>
     );
-   
+
 
   }
 }
 
 export default Image;
-// return <HornedBeast
-//                           handleOpen={this.props.handleOpen}
-//                           title={element.title}
-//                           image_url={element.image_url}
-//                            description ={element.description}
-//                           keyword = {element.keyword}
-//                           horns={element.horns}/>
-
-// {element.horns == 2 ? <HornedBeast
-//     handleOpen={this.props.handleOpen}
-//     title={element.title}
-//     image_url={element.image_url}
-//      description ={element.description}
-//     keyword = {element.keyword}
-//     horns={element.horns}/>
-//     :console.log('no');}
-
-//   }
-// )
-// }
